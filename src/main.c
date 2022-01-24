0420a8bb63976b8c397bc79b011f3a81479c1dbb
fonsi <alfonso.alfurtx@gmail.com>
2022-01-24 20:58:12 +0100

    WIP

--- src/main.c
+++ src/main.c
@@ -3,24 +3,28 @@
 
 internal void
 init()
-{}
+{
+}
 
 internal void
 destroy()
-{}
+{
+}
 
 internal void
 update()
-{}
+{
+}
 
 internal void
 render()
-{}
+{
+}
 
 int
 main(void)
 {
         window_init(init, destroy, update, render);
         window_loop();
-        return(0);
+        return (0);
 }
