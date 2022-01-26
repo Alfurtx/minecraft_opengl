23e593af195556179fbed7bb25a2487d74dce54b
fonsi <alfonso.alfurtx@gmail.com>
2022-01-26 15:31:11 +0100

    Añadido direcciones para chunks y blocks

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -4,4 +4,29 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+#include <cglm/cglm.h>
+
+// NOTE(fonsi): recordar que {0, 0, -1} es Norte
+enum Direction
+{
+        NORTH = 0,
+        SOUTH,
+        EAST,
+        WEST,
+        UP,
+        DOWN,
+        DIRECTION_COUNT
+};
+
+const vec3 DIRECTION_VEC[6] = {
+    { 0.0f,  0.0f, -1.0f},
+    { 0.0f,  0.0f,  1.0f},
+    { 1.0f,  0.0f,  0.0f},
+    {-1.0f,  0.0f,  0.0f},
+    { 0.0f,  1.0f,  0.0f},
+    { 0.0f, -1.0f,  0.0f},
+};
+
+#define DIRECTIONVEC(direction) (DIRECTION_VEC[direction])
+
 #endif // UTILS_H_
