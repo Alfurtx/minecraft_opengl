dd4e99959c047e259749e03c193dceb068701927
fonsi <alfonso.alfurtx@gmail.com>
2022-01-26 15:30:54 +0100

    Añadidos nuevos tipos

--- src/utils/types.h
+++ src/utils/types.h
@@ -1,7 +1,13 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stddef.h>
+
 typedef unsigned int uint;
+typedef uint64_t uint64;
+typedef uint8_t uint8;
 
 #define internal static
 
