af745b6c180180ce9a5dd0c49fc54e2599d19dc7
fonsiMac <alfonso.alfurtx@gmail.com>
2022-02-03 09:37:45 +0100

    Resuelto problema con el path en MACOS

--- src/gfx/renderer.c
+++ src/gfx/renderer.c
@@ -9,8 +9,8 @@ renderer_init(struct Renderer* renderer)
                 {
                 case RENDERER_CHUNK:
                 #ifdef __APPLE__
-                        renderer->shaders[i]  = shader_create("/Usera/alfonso/Desktop/Proyectos\ Personales/minecraft_opengl/res/shaders/chunk.vert", "/Usera/alfonso/Desktop/Proyectos\ Personales/minecraft_opengl/res/shaders/chunk.frag");
-                        renderer->textures[i] = texture_create("/Usera/alfonso/Desktop/Proyectos\ Personales/minecraft_opengl/res/textures/blockatlas.png");
+                        renderer->shaders[i]  = shader_create("/Users/alfonso/Desktop/Proyectos Personales/minecraft_opengl/res/shaders/chunk.vert", "/Users/alfonso/Desktop/Proyectos Personales/minecraft_opengl/res/shaders/chunk.frag");
+                        renderer->textures[i] = texture_create("/Users/alfonso/Desktop/Proyectos Personales/minecraft_opengl/res/textures/blockatlas.png");
                 #else
                         renderer->shaders[i]  = shader_create("/home/fonsi/proyectos/minecraft_opengl/res/shaders/chunk.vert", "/home/fonsi/proyectos/minecraft_opengl/res/shaders/chunk.frag");
                         renderer->textures[i] = texture_create("/home/fonsi/proyectos/minecraft_opengl/res/textures/blockatlas.png");
